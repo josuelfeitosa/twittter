5ba20fe47219d06164c980c5e51d092c31c7f117
Josuel Feitosa <josuelfeitosa@gmail.com>
2022-03-26 15:36:31 -0300

    Installed aplication gems and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "tweeets#index"
+
   resources :tweeets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
